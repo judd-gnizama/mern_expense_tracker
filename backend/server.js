1a51398490af651624451f1762fde7f2c538a985
judd.gnizama <juddmaverick@gmail.com>
2024-08-14 10:15:02 +0800

    edited start server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { testRouter } from "./routes/testRouter.js";
+import startServer from "./utils/startServer.js";
 
 const app = express();
 let PORT = 5000;
@@ -20,18 +21,3 @@ try {
 } catch (error) {
   console.error(error);
 }
-
-const startServer = (port) => {
-  app
-    .listen(port, () => {
-      console.log(`Listening on PORT ${port}`);
-    })
-    .on("error", (err) => {
-      if (err.code === "EADDRINUSE") {
-        console.error(`PORT ${port} is already in use. Trying next port...`);
-        startServer(port + 1);
-      } else {
-        console.error(err);
-      }
-    });
-};
