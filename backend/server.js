e13c59bd6a75dfc4ee500687c0d30cd0a86109ad
judd.gnizama <juddmaverick@gmail.com>
2024-08-14 15:19:47 +0800

    modified

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //Routes
 app.get("/", (req, res) => {
   res.json({
-    message: path.resolve(__dirname, "../frontend/dist", "index.html"),
+    message: "Heylow",
   });
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>hello</div>;
+  const [message, setMessage] = useState("");
+
+  const fetchData = async () => {
+    const res = await fetch("/api/test");
+    const data = await res.json();
+    return data.message;
+  };
+
+  useEffect(() => {
+    setMessage(fetchData());
+  }, []);
+
+  return <div>Hello {message}</div>;
 }
 
 export default App;
