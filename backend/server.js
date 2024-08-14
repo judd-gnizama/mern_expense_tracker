173d15d10d053ace5c389183b1db079930f7e634
judd.gnizama <juddmaverick@gmail.com>
2024-08-14 14:44:54 +0800

    up16

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,11 @@ app.get("/", (req, res) => {
 
 app.use("/api/test", testRouter);
 
+// Serve static files from the frontend build directory
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/dist", "/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 // Connect to database
