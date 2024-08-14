e33d77fbcd609b8fda7bd8272acc01dd79304233
judd.gnizama <juddmaverick@gmail.com>
2024-08-14 12:04:54 +0800

    up9

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 app.use("/api/test", testRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./frontend/dist", "index.html"));
 });
 
 // Connect to database
